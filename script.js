781ad6721e7b374e0a7d5d9b5208cf811b5d7654
Lozano Elian <Elian.lozano@outlook.fr>
2019-09-30 14:36:45 +0200

    azfzefz

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 var generate = document.getElementsByTagName("button")[0];
 
-generate.addEventListener("click" , function(){
-    var saisis = document.getElementById("saisis")
-    var nb = saisis.value ;
-    
 var gameDiv = document.createElement("div");
-gameDiv.setAttribute("id" , "game");
+gameDiv.setAttribute("id", "game");
 document.body.appendChild(gameDiv);
 
-var numbers = [];
 
-for (let i = 0; i < nb/2 ; i++) {
-    numbers.push(i+1);
-    numbers.push(i+1);
-}
-for (i = 0; i < numbers.length; i++) {
-    var random_index = Math.floor(Math.random() * (i + 1));
-    var temp = numbers[i];
-    numbers[i] = numbers[random_index];
-    numbers[random_index] = temp;
-}
-for (let i = 0; i < numbers.length; i++) {
-    var cellule = document.createElement("div");
-    cellule.className = "cell hidden"
-    gameDiv.appendChild(cellule)
-    cellule.innerHTML = numbers[i]
-    cellule.addEventListener("click" , function(){
-        this.className = "cell"
-    })
-}
+generate.addEventListener("click", function () {
+
+    gameDiv.innerHTML = "";
+    
+    var saisis = document.getElementById("saisis")
+    var nb = saisis.value;
+
+    var numbers = [];
+
+    for (let i = 0; i < nb / 2; i++) {
+        numbers.push(i + 1);
+        numbers.push(i + 1);
+    }
+    for (i = 0; i < numbers.length; i++) {
+        var random_index = Math.floor(Math.random() * (i + 1));
+        var temp = numbers[i];
+        numbers[i] = numbers[random_index];
+        numbers[random_index] = temp;
+    }
+    for (let i = 0; i < numbers.length; i++) {
+        var cellule = document.createElement("div");
+        cellule.className = "cell hidden"
+        gameDiv.appendChild(cellule)
+        cellule.innerHTML = numbers[i]
+        cellule.addEventListener("click", function () {
+            this.className = "cell"
+        })
+    }
 })
