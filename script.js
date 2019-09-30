b24feca64c60972f7449cfb2dda49daa32e2364c
tomsirat <sirat.tom@outlook.com>
2019-09-30 12:15:14 +0200

    BORDEL quand on rentre sur la piste

--- script.js
+++ script.js
@@ -2,13 +2,24 @@ var gameDiv = document.createElement("div");
 gameDiv.setAttribute("id" , "game");
 document.body.appendChild(gameDiv);
 
-var numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
-for (i = 0; i < numbers.length; i++) {
-    var random_index = Math.floor(Math.random() * (i + 1));
-    var temp = numbers[i];
+var numbers = [];
+var nb = 16;
+for (let i = 0; i < nb ; i++) {
+    var random_index = Math.floor(Math.random(0 , 8) * (i + 1));
+    var temp = [i] ;
+    
     numbers[i] = numbers[random_index];
     numbers[random_index] = temp;
+
+    
 }
+// var numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
+// for (i = 0; i < numbers.length; i++) {
+//     var random_index = Math.floor(Math.random() * (i + 1));
+//     var temp = numbers[i];
+//     numbers[i] = numbers[random_index];
+//     numbers[random_index] = temp;
+// }
 
 for (let i = 0; i < numbers.length; i++) {
     var cellule = document.createElement("div");
